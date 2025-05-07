530a541e3026cb9d12055da056d8276702eccaca
izmikomar <komar.izmi@gmail.com>
2025-05-07 18:47:15 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
