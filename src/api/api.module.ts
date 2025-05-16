d100fae44cbb3074909ab33f3c337a38b69577c5
izmikomar <komar.izmi@gmail.com>
2025-05-16 17:17:01 +0700

    style: reorder imports for improved organization and readability

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,13 +1,20 @@
+import { AuthModule } from './auth/auth.module';
+import { HasRolesModule } from './has-roles/has-roles.module';
 import { Module } from '@nestjs/common';
 import { PermissionsModule } from './permissions/permissions.module';
 import { ResourcesModule } from './resources/resources.module';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
-import { HasRolesModule } from './has-roles/has-roles.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, RolesModule, ResourcesModule, PermissionsModule, HasRolesModule, AuthModule],
+  imports: [
+    UsersModule,
+    RolesModule,
+    ResourcesModule,
+    PermissionsModule,
+    HasRolesModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
