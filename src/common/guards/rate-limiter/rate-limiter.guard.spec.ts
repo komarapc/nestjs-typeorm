2dce85b66f4c1a8846470e0b294f73f639c36314
izmikomar <komar.izmi@gmail.com>
2025-05-17 18:37:44 +0700

    refactor: remove unused test file for RateLimiterGuard

--- src/common/guards/rate-limiter/rate-limiter.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RateLimiterGuard } from './rate-limiter.guard';
-
-describe('RateLimiterGuard', () => {
-  it('should be defined', () => {
-    expect(new RateLimiterGuard()).toBeDefined();
-  });
-});
