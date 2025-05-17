9863298f0987e946d8fc0c7894c6c7eedfda2001
izmikomar <komar.izmi@gmail.com>
2025-05-17 21:19:35 +0700

    feat: add SitesEntity for site management in the database

---
+++ src/database/entity/sites.entity.ts
@@ -0,0 +1,22 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('sites')
+export class SitesEntity {
+  @PrimaryColumn({ length: 36 })
+  id: string;
+  @Column()
+  name: string;
+  @CreateDateColumn({ type: 'timestamptz' })
+  created_at: Date;
+  @UpdateDateColumn({ type: 'timestamptz', nullable: true })
+  updated_at?: Date;
+  @DeleteDateColumn({ type: 'timestamptz', nullable: true })
+  deleted_at?: Date;
+}
