9e2b8fa2a2df589be447bd3a4a7f4fddda125565
izmikomar <komar.izmi@gmail.com>
2025-05-18 18:54:20 +0700

    feat: add PermissionsEntity and PermissionsRepository to SitesModule for enhanced functionality

--- src/api/sites/sites.module.ts
+++ src/api/sites/sites.module.ts
@@ -1,4 +1,7 @@
+import { JwtService } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
+import { PermissionsEntity } from '@/database/entity/permissions.entity';
+import { PermissionsRepository } from '../permissions/permissions.repository';
 import { SitesController } from './sites.controller';
 import { SitesEntity } from '@/database/entity/sites.entity';
 import { SitesRepository } from './sites.repository';
@@ -6,8 +9,8 @@ import { SitesService } from './sites.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SitesEntity])],
+  imports: [TypeOrmModule.forFeature([SitesEntity, PermissionsEntity])],
   controllers: [SitesController],
-  providers: [SitesService, SitesRepository],
+  providers: [JwtService, SitesService, SitesRepository, PermissionsRepository],
 })
 export class SitesModule {}
